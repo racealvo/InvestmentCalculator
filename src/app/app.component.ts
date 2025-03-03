5c3e763f01e704cd8c6edc928de1a3aabd5bc765
Ray Salvo <salvo.raymond@gmail.com>
2025-03-03 18:09:24 -0500

    Working Investment Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { Component, Input } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentData } from './investment-data.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
+
   onCalculateInvestmentResults(data: InvestmentData) {
     const { initialInvestment, expectedReturn, duration, annualInvestment } = data;
     const annualData = [];
@@ -31,7 +41,7 @@ export class AppComponent {
       });
     }
   
-    return annualData;
+    this.resultsData = annualData;
   }
 
 }
